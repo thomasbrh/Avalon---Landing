de95a7fa671ae2c9f0826d78f9937631f1a9f2d3
thomasbrh <thomas.bruch@student.heaj.be>
2026-05-29 04:24:12 +0200

    Page crédits
    intégration page crédits

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { initHero } from './three.js/hero.js';
 
+/* anti-onglets */
+window.name = "_blank__landing";
+
 /* hero */
 if (document.getElementById('hero-canvas')) 
 {
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    // Déclaration explicite des pages pour le build multi-pages
-    rollupOptions: {
-      input: {
-        main:    resolve(__dirname, 'src/index.html'),
-        credits: resolve(__dirname, 'src/credits.html'),
-      },
+export default defineConfig(
+{
+    root: 'src',
+    publicDir: '../public',
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-  },
+    build: 
+    {
+        outDir: '../dist',
+        rollupOptions: 
+        {
+            input:
+            {
+                main:    resolve(__dirname, 'src/index.html'),
+                credits: resolve(__dirname, 'src/credits.html'),
+                ia:      resolve(__dirname, 'src/ia.html'),
+            },
+        },
+    },
 })
